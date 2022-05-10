ce1d10bac271d4f7677a9ea546112130ace835dd
philipjanson <ph8671ja-s@student.lu.se>
2022-05-10 20:49:57 +0200

    Website test

--- webserver/database.py
+++ webserver/database.py
@@ -9,8 +9,10 @@ CORS(app)
 
 # change this to connect to your redis server
 # ===============================================
-redis_server = redis.Redis("localhost", decode_responses=True, charset="unicode_escape")
+redis_server = redis.Redis(
+    "localhost", decode_responses=True, charset="unicode_escape")
 # ===============================================
+
 
 @app.route('/drone', methods=['POST'])
 def drone():
@@ -21,7 +23,7 @@ def drone():
     drone_latitude = drone['latitude']
     drone_status = drone['status']
     # Get the infomation of the drone in the request, and update the information in Redis database
-    # Data that need to be stored in the database: 
+    # Data that need to be stored in the database:
     # Drone ID, logitude of the drone, latitude of the drone, drone's IP address, the status of the drone
     # Note that you need to store the metioned infomation for all drones in Redis, think carefully how to store them
     # =========================================================================================
@@ -30,10 +32,9 @@ def drone():
     redis_server.set(droneID + '_latitude', drone_latitude)
     redis_server.set(droneID + '_status', drone_status)
 
-     # =======================================================================================
+    # =======================================================================================
     return 'Get data'
 
+
 if __name__ == "__main__":
-
-
     app.run(debug=True, host='0.0.0.0', port='5001')
